66dad68566074e026a4e947d6b27408e00dc7e97
Ivan Nikolov <44241004+ivanNikolov2910@users.noreply.github.com>
2020-06-24 11:47:12 +0300

    Add files via upload

---
+++ take-your-fine/src/test/java/com/example/takeyourfine/TakeYourFineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takeyourfine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeYourFineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
